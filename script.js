187948562afa844f358b08353f33c2f39be051d2
Noyz <julian.noyon@gmail.com>
2015-03-31 21:54:45 +0200

    first step js

---
+++ script.js
@@ -0,0 +1,6 @@
+		//Mais ici ca ne fonctionne pas je ne sais pas pourqoi// 
+		$(document).ready(function() {
+    $(".ninjahtml").mouseenter(function() {
+    	$(this).fadeOut(1000);
+});	
+    });
