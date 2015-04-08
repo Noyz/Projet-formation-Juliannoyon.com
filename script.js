bd825488403a3de15f7b3a5a9c458810d5ca4cc6
Noyz <julian.noyon@gmail.com>
2015-04-08 19:26:29 +0200

    jQuery sur cv.html

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
-$(document).ready(function() {
-	
-}
-
+$(document).ready(function(){
+    $("button").click(function(){
+        $("li").hide(1000);
+    });
+   
+});
 		
+		$("li").hide(function(){
+			 $("button").click(function(){
+        $("li").show(1000);
+    });
+			 });
