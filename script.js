ae4629d2ef249405a3d71d7593016e7228f0a771
Noyz <julian.noyon@gmail.com>
2015-04-09 01:41:52 +0200

    MAJ paufinage

--- script.js
+++ script.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
     $("button").click(function(){
         $("li").hide(1000);
     });
-   
+
+   $("button").toggle(function(){
+   	$('li').show(1000;)
+   })
+
 });
-		
-		$("li").hide(function(){
-			 $("button").click(function(){
-        $("li").show(1000);
-    });
-			 });
+
