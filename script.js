df606ca6a776cf81a3f7d62e42e9bfa48bf4e305
Noyz <julian.noyon@gmail.com>
2015-04-14 17:18:53 +0200

    Maj 14-04 BH

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
    $('button').click(function(){
    	$('#toHide').toggle(1000);
    });
+   $(".njlogo").click(function() {
+   	window.location.replace("index.html");
+   });
 });
 
