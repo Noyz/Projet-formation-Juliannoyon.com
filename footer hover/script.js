4312b8aed021effbb7f2f7c953a32c14c78cc864
Noyz <julian.noyon@gmail.com>
2015-04-23 12:41:38 +0200

    maj 2304

--- footer hover/script.js
+++ footer hover/script.js
@@ -1,8 +1,8 @@
 $('document').ready(function() {
 	$('.row1').mouseenter(function(){
-		$('.hide').show(1000);
+		$('.hide > .tier').show(1000);
 	});
-	$('.tier').mouseleave(function(){
-		$('.hide').hide(1000);
+	$('.tier1').mouseleave(function(){
+		$('.tier1 >.hide').hide(1000);
 	});
 });
