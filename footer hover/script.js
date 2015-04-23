35fc5c6840c77bb95c486b09c6a65cef026cc972
Noyz <julian.noyon@gmail.com>
2015-04-23 16:39:00 +0200

    blocked!

--- footer hover/script.js
+++ footer hover/script.js
@@ -1,8 +1,15 @@
 $('document').ready(function() {
-	$('.row1').mouseenter(function(){
-		$('.hide > .tier').show(1000);
+	$('.picture').mouseenter(function(){
+		$('.hide1').show(1000);
 	});
-	$('.tier1').mouseleave(function(){
-		$('.tier1 >.hide').hide(1000);
+	$('.picture').mouseleave(function(){
+		$('.hide1').hide(1000);
+	});
+
+	$('.picture').mouseenter(function(){
+		$('.hide2').show(1000);
+	});
+	$('.picture').mouseleave(function(){
+		$('.hide2').hide(1000);
 	});
 });
