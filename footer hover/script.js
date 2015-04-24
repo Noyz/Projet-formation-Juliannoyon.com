e767431cfa38ccde6d5dcc73e7a4044c02ce13af
Noyz <julian.noyon@gmail.com>
2015-04-24 10:26:39 +0200

    succeed

--- footer hover/script.js
+++ footer hover/script.js
@@ -6,10 +6,53 @@ $('document').ready(function() {
 		$('.hide1').hide(1000);
 	});
 
-	$('.picture').mouseenter(function(){
+	$('.picture2').mouseenter(function(){
 		$('.hide2').show(1000);
 	});
-	$('.picture').mouseleave(function(){
+	$('.picture2').mouseleave(function(){
 		$('.hide2').hide(1000);
+	});
+
+	$('.picture3').mouseenter(function(){
+		$('.hide3').show(1000);
+	});
+	$('.picture3').mouseleave(function(){
+		$('.hide3').hide(1000);
+	});
+	$('.picture4').mouseenter(function(){
+		$('.hide4').show(1000);
+	});
+	$('.picture4').mouseleave(function(){
+		$('.hide4').hide(1000);
+	});
+	$('.picture5').mouseenter(function(){
+		$('.hide5').show(1000);
+	});
+	$('.picture5').mouseleave(function(){
+		$('.hide5').hide(1000);
+	});
+	$('.picture6').mouseenter(function(){
+		$('.hide6').show(1000);
+	});
+	$('.picture6').mouseleave(function(){
+		$('.hide6').hide(1000);
+	});
+	$('.picture7').mouseenter(function(){
+		$('.hide7').show(1000);
+	});
+	$('.picture7').mouseleave(function(){
+		$('.hide7').hide(1000);
+	});
+	$('.picture8').mouseenter(function(){
+		$('.hide8').show(1000);
+	});
+	$('.picture8').mouseleave(function(){
+		$('.hide8').hide(1000);
+	});
+	$('.picture9').mouseenter(function(){
+		$('.hide9').show(1000);
+	});
+	$('.picture9').mouseleave(function(){
+		$('.hide9').hide(1000);
 	});
 });
