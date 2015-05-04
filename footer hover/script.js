d4375a2c8cf5cf1446dccbbe17baafa799c64cbc
Noyz <julian.noyon@gmail.com>
2015-05-04 14:08:40 +0200

    end of exo

--- footer hover/script.js
+++ footer hover/script.js
@@ -1,12 +1,12 @@
-
-
 $('document').ready(function() {
 	$('.tier').mouseenter(function(){
 		$(this).find('.petitTexte .hide').show('slow');
 	});
 	$('.tier').mouseleave(function(){
-		$(this).find('petitTexte .hide').hide('slow');
+		$(this).find('.petitTexte .hide').hide('slow');
 	});
+
+	$('.tier').each(function(i){
 	if(i % 3 == 0) {
 		$(this).css({
 			'clear' : 'both'
