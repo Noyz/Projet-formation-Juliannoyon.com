a41134f04c9974c4d099a98d5956726ee4ac918e
Noyz <julian.noyon@gmail.com>
2015-05-13 16:46:19 +0200

    re

--- opacity/javascript.js
+++ opacity/javascript.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 	$('img').mouseenter(function(){
-		$('this').find('.description .hide').fadeIn('slow');
+		$('.hide').show('slow');
+	});
+	$('img').mouseleave(function(){
+		$('.hide').hide('slow');
 	});
 
+	$('img').mouseenter(function(){
+		$('this').find('.description .hide').fadeIn('slow');
+	});
+	$('img').mouseleave(function(){
+		$('this').find('.description .hide').fadeOut('slow');
 });
+
+	});
